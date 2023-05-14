82b1fbab987fe0a1f3106034ebdd77153e28a12a
Szymon Jędrzejewski <szym335@gmail.com>
2023-05-14 12:38:55 +0200

    5 projects moved to another directory

--- springboot.projekt4.restapi.springdatajpa/src/main/java/pl/szymonjedrzejewski/springboot/projekt4/dao/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.szymonjedrzejewski.springboot.projekt4.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.szymonjedrzejewski.springboot.projekt4.entity.Player;
-
-public interface PlayerRepository extends JpaRepository <Player, Integer> {
-	
-	
-}
